c798fc3a61e58e08396cd688d69db3be46cdc1f4
lalatendu11 <lalatendung@gmail.com>
2024-04-12 09:10:27 +0530

    Method values Modified

--- src/apr12/Demo.java
+++ src/apr12/Demo.java
@@ -3,19 +3,19 @@ package apr12;
 public class Demo {
 public void add()
 {
-	int a=34,b=56,c;
+	int a=567856,b=8787,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=678,b=7,c;
+	int a=8787,b=886,c;
 	c=a/b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int a=678,b=90,c;
+	int a=76765,b=686,c;
 	c=a*b;
 	System.out.println(c);
 }

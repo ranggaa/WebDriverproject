ed82ec6effb03a75770f9226b6cb3f4188ef2f9c
lalatendu11 <lalatendung@gmail.com>
2024-04-12 09:08:05 +0530

    New Class With methods Added

---
+++ src/apr12/Demo.java
@@ -0,0 +1,22 @@
+package apr12;
+
+public class Demo {
+public void add()
+{
+	int a=34,b=56,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=678,b=7,c;
+	c=a/b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=678,b=90,c;
+	c=a*b;
+	System.out.println(c);
+}
+}
